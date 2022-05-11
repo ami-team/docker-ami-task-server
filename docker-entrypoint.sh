53ad3561c76b47742500f4ac77a8cf8c8700df55
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-05-11 17:18:54 +0200

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,21 +16,31 @@ AMI_CLASSPATH=$(find /AMITaskServer/lib/ -name '*.jar' | xargs echo | tr ' ' ':'
 ########################################################################################################################
 
 java \
+-server \
+\
 -Dami.server_name="${SERVER_NAME}" \
+\
 -Dami.mqtt_url="${MQTT_URL}" \
 -Dami.mqtt_username="${MQTT_USERNAME}" \
 -Dami.mqtt_password="${MQTT_PASSWORD}" \
+\
 -Dami.jdbc_url="${JDBC_URL}" \
 -Dami.jdbc_username="${JDBC_USERNAME}" \
 -Dami.jdbc_password="${JDBC_PASSWORD}" \
+\
 -Dami.exclusion_server_url="${EXCLUSION_SERVER_URL}" \
+\
 -Dami.cleanup_time="${CLEANUP_TIME}" \
 -Dami.quality_of_service="${QUALITY_OF_SERVICE}" \
 -Dami.max_nb_of_running_tasks="${MAX_NB_OF_RUNNING_TASKS}" \
 -Dami.priority_probability_ratio="${PRIORITY_PROBABILITY_RATIO}" \
+\
 -Dami.conf_file=/AMITaskServer/workspace/AMI.xml \
+\
 -Dfile.encoding=UTF-8 -Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom \
+\
 -classpath "${AMI_CLASSPATH}" \
+\
 net.hep.ami.task.Main
 
 ########################################################################################################################
